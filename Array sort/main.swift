745477d174ca2c4972cf0be0f85e0952c679196c
20141105073ld <a20141105073@admindeimac-12.local>
2016-06-21 09:13:33 +0800

    Initial Commit

---
+++ Array sort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Array sort
+//
+//  Created by 20141105073 on 16/6/21.
+//  Copyright © 2016年 20141105073. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
