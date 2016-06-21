64d0ae9b12bd42d50473e49a1c97d453ce999a29
20141105073ld <a20141105073@admindeiMac-12.local>
2016-06-21 10:23:26 +0800

    Array sort
    首先创建一个数组，使用Array关键字来声明。由于swift语言有类型的推断，可以直接使用正确的数组字面量来初始化数组。在编写程序的过程中，使用
    count来直接读取数组的元素数量。利用for循环来进行排序，for 循环中再继续嵌套一个for循环。

--- Array sort/main.swift
+++ Array sort/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+
+var array = [15,28,1,33,25,76,7,3,5,9,299,54,44,80,23,67,88,42,11,55]
+
+print("排序前的值：")
+
+for item in array
+{
+    var ii = item
+    print(ii)
+}
+for i in 0 ..< array.count - 1{
+    for j in 0 ..< array.count - 1 - i{
+        if array [j] > array [j + 1]{
+            var temp = array [j + 1]
+            array [j + 1] = array [j]
+            array [j] = temp
+        
+        }
+    }
+}
+
+print("排序后的值：")
+for item in array
+{
+    var ii = item
+    print(ii)
+
+}
 
